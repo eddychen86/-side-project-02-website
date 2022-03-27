3c46774611d88158850c6b612f021cc120af86d1
eddychen86 <eddychen101020@gmail.com>
2022-03-27 23:52:44 +0800

    Update product page

--- webPage/src/components/product/download.vue
+++ webPage/src/components/product/download.vue
@@ -197,74 +197,59 @@ const product = "PhotoNFT";
 const dl = "Download";
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 header {
   padding: auto;
   color: #fff;
   background-color: rgba(0,0,0, 0.5);
   backdrop-filter: blur(10px);
-}
-.banner {
-  padding-top: 5vh;
-  padding-bottom: 5vh;
-}
-h1 {
-  font-size: 4rem;
-  font-weight: 900;
-}
-.btn {
-  letter-spacing: 2px;
-  font-weight: 900;
-  top: 50%;
-  transform: translateY(-50%);
-  font-size: 1.5rem;
-}
-.mask {
-  background: rgba(0 ,0 ,0 , 0);
-	transition: .3s;
-	cursor: pointer;
-	opacity: 0;
-  height: 100%;
-  width: 100%;
-}
-.mask:hover {
-  background: rgba(0 ,0 ,0 , 0.5);
-  opacity: 1;
-}
-
-.img1 {
-  background-image: url("../public/mark/index-01-mark.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  height: 15vh;
-}
-.img2 {
-  background-image: url("../public/mark/index-02-mark.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  height: 15vh;
-}
-.img3 {
-  background-image: url("../public/mark/index-03-mark.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  height: 15vh;
-}
-.img4 {
-  background-image: url("../public/mark/index-04-mark.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  height: 15vh;
-}
-.img5 {
-  background-image: url("../public/mark/index-05-mark.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  height: 15vh;
+  .banner {
+    padding-top: 5vh;
+    padding-bottom: 5vh;
+    h1 {
+      font-size: 4rem;
+      font-weight: 900;
+    }
+    .img1, .img2, .img3, .img4, .img5 {
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center;
+      height: 20vh;
+    }
+    .img1 {
+      background-image: url("../public/mark/index-01-mark.jpg");
+    }
+    .img2 {
+      background-image: url("../public/mark/index-02-mark.jpg");
+    }
+    .img3 {
+      background-image: url("../public/mark/index-03-mark.jpg");
+    }
+    .img4 {
+      background-image: url("../public/mark/index-04-mark.jpg");
+    }
+    .img5 {
+      background-image: url("../public/mark/index-05-mark.jpg");
+    }
+    .mask {
+      background: rgba(0 ,0 ,0 , 0);
+      transition: .3s;
+      cursor: pointer;
+      opacity: 0;
+      height: 100%;
+      width: 100%;
+      &:hover {
+        background: rgba(0 ,0 ,0 , 0.5);
+        opacity: 1;
+      }
+      .btn {
+        letter-spacing: 2px;
+        font-weight: 900;
+        top: 50%;
+        transform: translateY(-50%);
+        font-size: 1.5rem;
+      }
+    }
+  }
 }
 </style>
